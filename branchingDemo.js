a203cae6175aa72a3c4b0031d5d6fa1f32765389
spoutshield <shojha@ctemc.org>
2022-09-09 12:24:33 -0400

    Create branchingDemo.js
    Added branchingDemo.js file.

---
+++ branchingDemo.js
@@ -0,0 +1,11 @@
+// Filename: branchingDemo.js
+// Author: S. Ojha
+// Objective: Demonstrate how to use GitHub branches
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+const d = new Date();
+
+let month = months[d.getMonth()];
+
+console.log("The date is: " + d);
