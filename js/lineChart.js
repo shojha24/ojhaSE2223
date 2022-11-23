64c691b617f880f6f0c16cfe8e7f5473fdbc681d
shojha24 <79208016+shojha24@users.noreply.github.com>
2022-11-22 22:15:26 -0500

    Reversion of All Commits of 11/20
    Don't know what happened, but I've reverted everything back to what it was pre-11/20 and it works on live server.

--- js/lineChart.js
+++ js/lineChart.js
@@ -1,7 +1,7 @@
 // GRaph CSV with Chart.JS
 
 async function getData() {
-    const response = await fetch("researchData.csv")
+    const response = await fetch("data/researchData.csv")
     const data = await response.text()          //CSV in TEXT format
 
     const table = data.split('\r\n').slice(1);  // split by line and remove 0th row.
